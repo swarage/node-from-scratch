7328748b1b17119f2178d5a480bdacc73934e9cd
swarage <sdcoolio@gmail.com>
2015-07-21 20:49:59 -0400

    added event listeners for the ls process
    I added two event listeners, one to check and see if any data was
    passed to stdout from ls, and one to see when the process is closed
    from completion of ls. For the first listener, it appends the output
    to a variable called 'output.' For the second listener, it displays
    the output to stdout in our process, or the process of our node
    program

--- filesystems/watcher.js
+++ filesystems/watcher.js
@@ -9,8 +9,17 @@ if (!filename) {
 }
 
 fs.watchFile(filename, function() {
-  let ls = spawn('ls', ['-lh', filename]);
-  ls.stdout.pipe(process.stdout);
+  let ls = spawn('ls', ['-lh', filename]),
+      output = '';
+
+  ls.stdout.on('data', function(buffer) {
+    output += buffer.toString();
+  });
+
+  ls.on('close', function() {
+    let parts = output.split(/\s+/);
+    console.dir([parts[0], parts[4], parts[8]]);
+  })
 });
 
 console.log('now watching ' + filename + ' for changes...');
